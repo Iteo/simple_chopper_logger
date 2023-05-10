027381e63c32b0cbe1598e7eae669a34eca6c838
Mateusz Filipek <mateusz.filipek@iteo.com>
2023-05-10 13:13:44 +0200

    rc/1.0.0

--- lib/simple_chopper_logger.dart
+++ lib/simple_chopper_logger.dart
@@ -1,18 +1,31 @@
+/// Implementation of Request and Response Chopper interceptors that provides simple but pleasant to look at output.
 library simple_chopper_logger;
 
 import 'dart:async';
 import 'dart:convert';
 import 'package:chopper/chopper.dart';
 
+/// A function that is used to log all data.
+/// By default, it uses [print].
+///
+/// To change the default behavior, pass any function that takes a [String] as an argument.
 typedef Logger = void Function(String line);
-
-extension YYYYMMDDHHNNSS on DateTime {
-  String get yyyymmddhhnnss =>
-      '$year-${month.toString().padLeft(2, '0')}-${day.toString().padLeft(2, '0')} - ${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}:${second.toString().padLeft(2, '0')}';
-}
 
 const _prefix = '[SimpleChopperLogger] ';
 
+/// A [RequestInterceptor] and [ResponseInterceptor] that logs requests and responses.
+///
+/// Logging of specific parts of the request and response can be enabled or disabled
+/// by using the appropriate constructor parameters.
+///
+/// The default logging method is [print], but this can be changed by passing a custom
+/// [Logger] to the constructor.
+///
+/// Default prefix added to each log line is `'[SimpleChopperLogger] '`, but this can
+/// be changed by passing a custom prefix to the constructor.
+///
+/// **Warning** Be careful when using this in production, as it will log sensitive
+/// information such as headers and request/response bodies.
 class SimpleChopperLogger implements RequestInterceptor, ResponseInterceptor {
   SimpleChopperLogger({
     this.includeRequestHeaders = true,
@@ -23,11 +36,24 @@ class SimpleChopperLogger implements RequestInterceptor, ResponseInterceptor {
     this.logger = print,
   });
 
+  /// Specifies whether request headers should be logged.
   final bool includeRequestHeaders;
+
+  /// Specifies whether request body should be logged.
   final bool includeRequestBody;
+
+  /// Specifies whether response headers should be logged.
   final bool includeResponseHeaders;
+
+  /// Specifies whether response body should be logged.
   final bool includeResponseBody;
+
+  /// Specifies the prefix that will be added to each log line.
   final String prefix;
+
+  /// Specifies the logger that will be used to log each line.
+  ///
+  /// By default, it uses [print].
   final Logger logger;
 
   @override
@@ -71,7 +97,7 @@ class SimpleChopperLogger implements RequestInterceptor, ResponseInterceptor {
   }
 
   @override
-  FutureOr<Response> onResponse(Response response) {
+  FutureOr<Response<dynamic>> onResponse(Response<dynamic> response) {
     final request = response.base.request;
     final output = StringBuffer();
 
@@ -127,4 +153,11 @@ class SimpleChopperLogger implements RequestInterceptor, ResponseInterceptor {
 
     return response;
   }
+}
+
+/// Extension on [DateTime] that returns a formatted string.
+/// Format: `YYYY-MM-DD - HH:NN:SS`
+extension _YYYYMMDDHHNNSS on DateTime {
+  String get yyyymmddhhnnss =>
+      '$year-${month.toString().padLeft(2, '0')}-${day.toString().padLeft(2, '0')} - ${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}:${second.toString().padLeft(2, '0')}';
 }
